bd5ba210584616af91df568760fc1fcdf18e8d79
Sivin1 <vincentchinomso20@gmail.com>
2022-12-10 01:06:40 +0100

    print type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,13 @@ int main(void)
 	char charType;
 	int intType;
 	long int longIntType;
-	long long longLongType;
+	long long int longLongType;
 	float floatType;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType))
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongType))
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longIntType));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longLongType));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 	return (0);
 }
