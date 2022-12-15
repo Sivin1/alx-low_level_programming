bfb1adb25b86618e4935d849a7757e4a92d8bbac
Sivin1 <vincentchinomso20@gmail.com>
2022-12-15 09:58:28 +0100

    draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - draw a straight line
+ * @n: the character
+ * Return: return 0
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
